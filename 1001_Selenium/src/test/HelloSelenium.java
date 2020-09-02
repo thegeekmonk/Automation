e1117bd2d74424e021a0088b6eb68e733f4f5056
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-02 16:40:22 +0530

    test

--- 1001_Selenium/src/test/HelloSelenium.java
+++ 1001_Selenium/src/test/HelloSelenium.java
@@ -1,12 +1,20 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HelloSelenium
 {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+          
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		driver.quit();
+		
 	}
 
 }
