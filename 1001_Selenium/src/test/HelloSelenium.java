8a85d31a365ad7d03ba50d5606df78c8747f3a79
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-02 16:58:45 +0530

    test

--- 1001_Selenium/src/test/HelloSelenium.java
+++ 1001_Selenium/src/test/HelloSelenium.java
@@ -9,7 +9,9 @@ public class HelloSelenium
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-          
+        
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\AkhileshPC\\Selenium\\1001_Selenium\\lib\\chromedriver\\chromedriver.exe");
+		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://google.com");
