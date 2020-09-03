1ac585aa81923736d389e47c38dd2d9ca262f776
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-03 16:18:01 +0530

    test externally

---
+++ 1002_Selenium_Properties/src/test/SeleniumTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+	public static String browser;
+	public static WebDriver driver;
+	
+	public static void setBrowser()
+	{
+		browser = "Chrome";
+	}
+	
+	public static void setBrowserConfig() {
+		
+		if(browser.equals("Chrome"))
+		{   
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\AkhileshPC\\Selenium\\1001_Selenium\\lib\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+		}
+		else if(browser.equals("Firefox"))
+		{
+			
+		}		
+		
+	}
+	
+	public static void runTest()
+	{
+		driver.get("https://youtube.com");
+		driver.close();
+	}
+		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+	    setBrowser();
+	    setBrowserConfig();
+	    runTest();
+		
+	}
+
+}
