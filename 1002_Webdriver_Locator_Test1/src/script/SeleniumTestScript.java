c7a919791a56364d470d75ccdb961703d2410a42
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-08 19:56:23 +0530

    Webdriver 1st test script

--- 1002_Webdriver_Locator_Test1/src/script/SeleniumTestScript.java
+++ 1002_Webdriver_Locator_Test1/src/script/SeleniumTestScript.java
@@ -1,16 +1,36 @@
 package script;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTestScript 
 {
 	 public static WebDriver driver; 
-	
-	
+	 public static List<WebElement> elements;
+	 
      public static void setConfiguration()
      {
     	 System.setProperty("webdriver.chrome.driver","C:\\Users\\AkhileshPC\\Selenium\\1002_Webdriver_Locator_Test1\\lib\\chromedriver\\chromedriver.exe");
     	 driver = new ChromeDriver();
+    	 driver.manage().window().maximize();
+    	 driver.manage().timeouts().implicitlyWait(7,TimeUnit.SECONDS);
+    	  
+     }
+     
+     public static void runTestScript()
+     {
+    	 driver.get("https://google.com");
+    	 
+    	 //To find the text input element of google search page
+    	 WebElement element = driver.findElement(By.name("q"));
+    	 element.sendKeys("YouTube");   	 
+    	 
+    	 elements = driver.findElements(By.partialLinkText("YouTube"));   	 
+    	 
      }
 }
--- 1002_Webdriver_Locator_Test1/src/test/SeleniumTest.java
+++ 1002_Webdriver_Locator_Test1/src/test/SeleniumTest.java
@@ -1,10 +1,35 @@
 package test;
 
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import script.SeleniumTestScript;
+
+@RunWith(Parameterized.class)
 public class SeleniumTest 
 {
-
-	public SeleniumTest() {
+    private SeleniumTestScript selenium;
+    
+	public SeleniumTest() 
+	{
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Before
+	public void initialize()
+	{
+		selenium = new SeleniumTestScript();
+	}
+	
+	@Parameterized.Parameters
+	public static Collection input()
+	{
+		return SeleniumTestScript.elements;
+	}
+	
+	
 
 }
