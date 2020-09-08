ca60eb8f00f1f7855a5347fe3aeeea8f1ed01113
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-08 19:13:15 +0530

    Webdriver 1st test script

--- 1002_Webdriver_Locator_Test1/src/script/SeleniumTestScript.java
+++ 1002_Webdriver_Locator_Test1/src/script/SeleniumTestScript.java
@@ -1,6 +1,16 @@
 package script;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumTestScript 
 {
-     
+	 public static WebDriver driver; 
+	
+	
+     public static void setConfiguration()
+     {
+    	 System.setProperty("webdriver.chrome.driver","C:\\Users\\AkhileshPC\\Selenium\\1002_Webdriver_Locator_Test1\\lib\\chromedriver\\chromedriver.exe");
+    	 driver = new ChromeDriver();
+     }
 }
