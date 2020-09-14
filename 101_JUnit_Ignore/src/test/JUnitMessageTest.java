ce88df6db10f132a901e2f0ec402ad0c5c2bfbd4
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-14 22:00:49 +0530

    @Ignore Annotataion

--- 101_JUnit_Ignore/src/test/JUnitMessageTest.java
+++ 101_JUnit_Ignore/src/test/JUnitMessageTest.java
@@ -1,6 +1,24 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import script.JUnitMessage;
+
 public class JUnitMessageTest 
 {
-
+    String message = "Hello";
+	
+	private JUnitMessage junitMsg = new JUnitMessage(message);
+	
+	@Test
+	public void testPrintMessage()
+	{
+		System.out.println("Inside Printing Message");
+		
+		assertEquals(message,junitMsg.printMessage());
+	}
+	
+	
 }
