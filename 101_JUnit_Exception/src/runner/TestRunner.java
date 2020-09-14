1caab1a051d678e424325cdf6d418d37edbe0abe
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-14 23:36:14 +0530

    Expected Exception

--- 101_JUnit_Exception/src/runner/TestRunner.java
+++ 101_JUnit_Exception/src/runner/TestRunner.java
@@ -1,14 +1,23 @@
 package runner;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import test.JUnitMessageTest;
+
 public class TestRunner {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 
+		Result result = JUnitCore.runClasses(JUnitMessageTest.class);
 		
+		for(Failure failure : result.getFailures())
+			System.out.println(failure.toString());
 		
-		
+		System.out.println(result.wasSuccessful());
 		
 	}
 
