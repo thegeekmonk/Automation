37ba5fac5fa0579bcb4f0b638af675d76edf93c9
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-15 18:55:59 +0530

    ErrorCollector  @Rule Annotation

--- 101_JUnit_ErrorCollector/src/runner/TestRunner.java
+++ 101_JUnit_ErrorCollector/src/runner/TestRunner.java
@@ -1,15 +1,23 @@
 package runner;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import test.ErrorTest;
+
 public class TestRunner {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 
+		Result result = JUnitCore.runClasses(ErrorTest.class);
 		
+		for(Failure failure : result.getFailures())
+			System.out.println(failure.toString());
 		
-		
-		
+		System.out.println(result.wasSuccessful());
 	}
 
 }
