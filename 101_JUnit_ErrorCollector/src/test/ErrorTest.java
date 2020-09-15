9df93dd5ad4f028c331666b804c1ed225886cc79
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-15 18:43:59 +0530

    ErrorCollector  @Rule Annotation

--- 101_JUnit_ErrorCollector/src/test/ErrorTest.java
+++ 101_JUnit_ErrorCollector/src/test/ErrorTest.java
@@ -1,10 +1,22 @@
 package test;
 
+import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
 public class ErrorTest 
 {
     @Rule    
 	ErrorCollector collector = new ErrorCollector();
+    
+    @Test
+    public void displayError()
+    {
+    	collector.addError(new Throwable("This is first error to be collected"));
+    	
+    	Assert.assertTrue("I love Algorithm".contains("Algotithm"));
+    }
+    
+    
 }
