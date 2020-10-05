ba74d86d4ff2e90d69782076668cb6d2cc9b2673
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-05 22:32:39 +0530

    webdriver POM test

---
+++ 1002_Webdriver_POM_1/src/runner/FacebookTestRunner.java
@@ -0,0 +1,14 @@
+package runner;
+
+public class FacebookTestRunner {
+
+	public FacebookTestRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 1002_Webdriver_POM_1/src/test/FacebookTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class FacebookTest {
+
+	public FacebookTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
