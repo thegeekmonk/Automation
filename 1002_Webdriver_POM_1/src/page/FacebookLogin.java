0685f3f74ecfa411776bb3e0d64986fe9849c32e
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-06 22:39:26 +0530

    webdriver POM test

--- 1002_Webdriver_POM_1/src/page/FacebookLogin.java
+++ 1002_Webdriver_POM_1/src/page/FacebookLogin.java
@@ -1,8 +1,10 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+
 public class FacebookLogin {
     
-	
+	WebDriver driver;
 	
 	public FacebookLogin() {
 		// TODO Auto-generated constructor stub
